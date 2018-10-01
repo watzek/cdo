c7347e93808f3fede0d1ce6619917a35804e2382
PatrickVin <38648924+PatrickVin@users.noreply.github.com>
2018-09-30 17:37:12 -0700

    Update LayerCustomize.js

--- src/MapPane/LayerCustomize.js
+++ src/MapPane/LayerCustomize.js
@@ -203,13 +203,17 @@ function onEachPOI(feature, layer) {
 	});
 }
 
-
-function onEachTribe(featuer, layer){
+function onEachTribe(feature, layer) {
 	layer.bindTooltip(feature.properties.Name);
 	layer.on({
 		click: () => {
 			const latOffset = -1.4;
 			context.props.changePane('info', feature.properties);
+			const ll = Leaflet.GeoJSON.coordsToLatLng([
+				feature.geometry.coordinates[0] + latOffset,
+				feature.geometry.coordinates[1]
+			]);
+			context.state.map.setView(ll, 7);
 		}
 	});
 }
