9af67cf3bd35c0ff65b48ba0fd71b0894d20bad4
PatrickVin <38648924+PatrickVin@users.noreply.github.com>
2018-10-07 18:44:12 -0700

    Update LayerCustomize.js

--- src/MapPane/LayerCustomize.js
+++ src/MapPane/LayerCustomize.js
@@ -206,17 +206,7 @@ function onEachPOI(feature, layer) {
 
 function onEachTribe(feature, layer) {
 	layer.bindTooltip(feature.properties.Tribe);
-	layer.on({
-		click: () => {
-			const latOffset = -1.4;
-			context.props.changePane('info', feature.properties);
-			const ll = Leaflet.GeoJSON.coordsToLatLng([
-				feature.geometry.coordinates[0] + latOffset,
-				feature.geometry.coordinates[1]
-			]);
-			context.state.map.setView(ll, 7);
-		}
-	});
+	
 }
 
 function onEachTrail(feature, layer) {
