a88ca8f5e7d29475136b70be93594b3fcff620c4
PatrickVin <38648924+PatrickVin@users.noreply.github.com>
2018-10-07 21:33:07 -0700

    Update LayerCustomize.js

--- src/MapPane/LayerCustomize.js
+++ src/MapPane/LayerCustomize.js
@@ -89,10 +89,7 @@ export function PointToLayer(name, cntxt) {
 
 function style1803(feature){
 	var color = '#000000';
-	if (
-		feature.properties.id.indexOf('1') >=0
-		
-	)
+	if (feature.properties.id.('1') >=0)
 		color = '#0000FF';
 	
 	return { fillColor: color, fillOpacity: 0.3, stroke: false };	
