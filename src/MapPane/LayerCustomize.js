2bb57c5d4ad5fc12867db5678000abafed7c29df
PatrickVin <38648924+PatrickVin@users.noreply.github.com>
2018-10-27 15:45:41 -0700

    Update LayerCustomize.js

--- src/MapPane/LayerCustomize.js
+++ src/MapPane/LayerCustomize.js
@@ -126,7 +126,7 @@ function pointToTribes(feature, latlng) {
 }
 
 function pointToPOI(feature, latlng) {
-	var color = marker_colors[categories.indexOf(feature.properties.Tribe)];
+	var color = marker_colors[categories.indexOf(feature.properties.Category)];
 	if (!color) color = 'map-marker-icon.png';
 
 	const teardrop = Leaflet.icon({
