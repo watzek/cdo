f4fa6683552088ce9ee628ace18a7d16d8c03a1f
PatrickVin <38648924+PatrickVin@users.noreply.github.com>
2018-11-04 19:29:23 -0800

    Update LayerCustomize.js

--- src/MapPane/LayerCustomize.js
+++ src/MapPane/LayerCustomize.js
@@ -24,7 +24,7 @@ import 'leaflet/dist/leaflet';
    integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA=="
    crossorigin=""/>
 	
- <script src="https://unpkg.com/leaflet@1.3.4/dist/leaflet.js" integrity="sha512-nMMmRyTVoLYqjP9hrbed9S+FzjZHW5gY1TWCHA5ckwXZBadntCNs8kEqAWdrb9O7rxbCaA4lKTIWjDXZxflOcA==" crossorigin=""></script>
+ <script> <src="https://unpkg.com/leaflet@1.3.4/dist/leaflet.js" integrity="sha512-nMMmRyTVoLYqjP9hrbed9S+FzjZHW5gY1TWCHA5ckwXZBadntCNs8kEqAWdrb9O7rxbCaA4lKTIWjDXZxflOcA==" crossorigin=""></script>
 
 
 const categories = [
