ad476ba0723e8def50d4ba78bd258790886e6099
PatrickVin <38648924+PatrickVin@users.noreply.github.com>
2018-11-04 17:19:22 -0800

    Update LayerCustomize.js

--- src/MapPane/LayerCustomize.js
+++ src/MapPane/LayerCustomize.js
@@ -16,6 +16,7 @@ many things without working under the hood a bit.
 */
 
 import Leaflet from 'leaflet';
+import 'leaflet/dist/leaflet.css' import 'leaflet/dist/leaflet' import 'leaflet-draw' import 'leaflet-draw/dist/leaflet.draw.css' import 'leaflet.markercluster' import 'leaflet.markercluster/dist/MarkerCluster.css';
 import * as React from 'react';
 import {
 	Map as LMap,
