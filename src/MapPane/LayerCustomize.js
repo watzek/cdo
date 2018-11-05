30f2d1df71e9af506a408a3e5f5771ec9027b28d
PatrickVin <38648924+PatrickVin@users.noreply.github.com>
2018-11-04 18:48:21 -0800

    Update LayerCustomize.js

--- src/MapPane/LayerCustomize.js
+++ src/MapPane/LayerCustomize.js
@@ -117,7 +117,7 @@ function style1803(feature, layer){
 }
 
 function onPolitical(feature, layer){
-	var marker = marker([51.5, -0.09]).addTo(layer);
+	var marker = marker.addTo(layer);
 	marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 	layer.bindPopup(feature.properties.NAME, {closeOnClick: false });
 }
