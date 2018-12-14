66e2bb931f84780e0212f900d3c592fa13e22b98
Quinn Vinlove <quinnvinlove@skylab.org>
2018-12-13 17:43:10 -0800

    Update credit menu

--- src/TopBar/credits.js
+++ src/TopBar/credits.js
@@ -100,150 +100,181 @@ export const about = [
 ,
 //PUT ABOUT TEXT BELOW HERE AS HTML
 <div>
-
 <p>
-        Allen, John Logan. 1975. <em>Passage through the Garden: Lewis and Clark
+      Allen, John Logan. 1975. <em>Passage through the Garden: Lewis and Clark
 and the Image of the American Northwest</em>. Urbana: University of Illinois
 Press.
 </p>
 <p>
-        ———. 1991. <em>Lewis and Clark and the Image of the American
+      ———. 1991. <em>Lewis and Clark and the Image of the American
 Northwest</em>. New York: Dover Publications.
 </p>
 <p>
-        Ambrose, Stephen E. 1997. <em>Undaunted Courage: Meriwether Lewis,
+      Ambrose, Stephen E. 1997. <em>Undaunted Courage: Meriwether Lewis,
 Thomas Jefferson, and the Opening of the American West</em>. First Touchstone
 edition. New York, NY: Simon & Schuster.
 </p>
 <p>
-        Beckham, Stephen Dow., Doug. Erickson, Jeremy. Skinner, Paul. Merchant,
+      Beckham, Stephen Dow., Doug. Erickson, Jeremy. Skinner, Paul. Merchant,
 and Or.) Lewis & Clark College (Portland. 2003. <em>The Literature of the Lewis
 and Clark Expedition: A Bibliography and Essays</em>. Portland, Or.: Lewis &
 Clark College.
 </p>
 <p>
-        Betts, Robert B. 2000. <em>In Search of York: The Slave Who Went to the
+      Betts, Robert B. 2000. <em>In Search of York: The Slave Who Went to the
 Pacific with Lewis and Clark</em>. Boulder: University Press of Colorado :
 </p>
 <p>
-        Cutright, Paul Russell. 1969. <em>Lewis and Clark, Pioneering
+      Cutright, Paul Russell. 1969. <em>Lewis and Clark, Pioneering
 Naturalists</em>. Urbana: University of Illinois Press.
 </p>
 <p>
-        Eide, Ingvard Henry., William Clark, Reuben Gold Thwaites, and
+      Eide, Ingvard Henry., William Clark, Reuben Gold Thwaites, and
 Meriwether Lewis. 1969. <em>American Odyssey: The Journey of Lewis and
 Clark</em>. Chicago: Rand McNally.
 </p>
 <p>
-        Fenster, J. M. 2017. <em>Jefferson's America: The President, the
+      Fenster, J. M. 2017. <em>Jefferson's America: The President, the
 Purchase, and the Explorers Who Transformed a Nation</em>. First paperback
 edition. New York: Broadway Books, an imprint of Crown Publishing Group, a
 division of Penguin Random House LLC.
 </p>
 <p>
-        Furtwangler, Albert. 1999. <em>Acts of Discovery: Visions of America in
+      Furtwangler, Albert. 1999. <em>Acts of Discovery: Visions of America in
 the Lewis and Clark Journals</em>. Urbana: University of Illinois Press.
 </p>
 <p>
-        Gilman, Carolyn, and Missouri Historical Society. 2003. <em>Lewis and
+      Gilman, Carolyn, and Missouri Historical Society. 2003. <em>Lewis and
 Clark: Across the Divide</em>. Washington, D.C.: Smithsonian Books ;
 </p>
 <p>
-        Hawke, David Freeman., and Meriwether Lewis. 1980. <em>Those Tremendous
+      Hawke, David Freeman., and Meriwether Lewis. 1980. <em>Those Tremendous
 Mountains: The Story of the Lewis and Clark Expedition</em>. 1st ed. New York:
 Norton.
 </p>
 <p>
-        Howard, Harold. 1971. <em>Sacajawea</em>. Norman, Oklahoma: University
+      Howard, Harold. 1971. <em>Sacajawea</em>. Norman, Oklahoma: University
 of Oklahoma Press.
 </p>
 <p>
-        Jackson, Donald. 1993. <em>Thomas Jefferson & the Stony Mountains:
+      Jackson, Donald. 1993. <em>Thomas Jefferson & the Stony Mountains:
 Exploring the West from Monticello</em>. Norman: University of Oklahoma Press.
 </p>
 <p>
-        Johnsgard, Paul A. 2003. <em>Lewis and Clark on the Great Plains: A
+      Johnsgard, Paul A. 2003. <em>Lewis and Clark on the Great Plains: A
 Natural History</em>. Lincoln: University of Nebraska Press, with the Center for
 Great Plains Studies, University of Nebraska-Lincoln.<a
 href="http://www.e-streams.com/es0703/es0703_3114.html">
 http://www.e-streams.com/es0703/es0703_3114.html</a>.
 </p>
 <p>
-        Jones, Landon Y. 2004. <em>William Clark and the Shaping of the
+      Jones, Landon Y. 2004. <em>William Clark and the Shaping of the
 West</em>. 1st ed. New York: Hill and Wang.
 </p>
 <p>
-        Josephy, Alvin M., and Marc. Jaffe. 2006. <em>Lewis and Clark through
+      Josephy, Alvin M., and Marc. Jaffe. 2006. <em>Lewis and Clark through
 Indian Eyes</em>. 1st ed. New York: Knopf.
 </p>
 <p>
-        Laliberte, Andrea, and William Ripple. 2003. "Wildlife Encounters by
+      Laliberte, Andrea, and William Ripple. 2003. "Wildlife Encounters by
 Lewis and Clark: A Spatial Analysis of Interactions between Native Americans and
 Wildlife." <em>Bio Science</em> 53 (10): 994–1003.<a
 href="https://doi.org/doi:10.1641/0006-3568(2003)053[0994:weblac]2.0.co;2">
 https://doi.org/doi:10.1641/0006-3568(2003)053[0994:weblac]2.0.co;2</a>.
 </p>
 <p>
-        Lavender, David. 1990. <em>The Way to the Western Sea: Lewis and Clark
+      Lavender, David. 1990. <em>The Way to the Western Sea: Lewis and Clark
 across the Continent</em>. 1st Anchor Books ed. New York: Anchor Books.
 </p>
 <p>
-        "Lewis & Clark Trail: Court Martials." n.d. Lewis and Clark Trail.<a
+      "Lewis & Clark Trail: Court Martials." n.d. Lewis and Clark Trail.<a
 href="http://lewisandclarktrail.com/courtmartial.htm">
 http://lewisandclarktrail.com/courtmartial.htm</a>.
 </p>
 <p>
-        Lewis, Meriwether, William Clark, Gary E. Moulton, Thomas W. Dunlay,
+      Lewis, Meriwether, William Clark, Gary E. Moulton, Thomas W. Dunlay,
 University of Nebraska--Lincoln. Center for Great Plains Studies., and American
 Philosophical Society. 1983. <em>The Journals of the Lewis and Clark
 Expedition</em>. Lincoln: University of Nebraska Press.
 </p>
 <p>
-        Nelson, Dale. 2003. <em>Interpreters with Lewis & Clark: The Story of
+      Nelson, Dale. 2003. <em>Interpreters with Lewis & Clark: The Story of
 Sacagawea and Toussaint Charboneau</em>. Denton, Texas: University of North
 Texas Press.
 </p>
 <p>
-        Nicandri, David L., and Clay. Jenkinson. 2009. <em>River of Promise:
+      Nicandri, David L., and Clay. Jenkinson. 2009. <em>River of Promise:
 Lewis and Clark on the Columbia</em>. Washburn, N.D.: Dakota Institute Press of
 the Lewis & Clark Fort Mandan Foundation.
 </p>
 <p>
-        O'Neill, Jeanne. 1998. <em>Lewis & Clark in the Bitterroot</em>.
+      O'Neill, Jeanne. 1998. <em>Lewis & Clark in the Bitterroot</em>.
 Stevensville, Mont.: Stoneydale Press.
 </p>
 <p>
-        Ronda, James P. 1988. <em>Lewis and Clark among the Indians</em>.
+      Ronda, James P. 1988. <em>Lewis and Clark among the Indians</em>.
 Lincoln, NB ; University of Nebraska Press.
 </p>
 <p>
-        ———. 2000. <em>Jefferson's West: A Journey with Lewis and Clark</em>.
+      ———. 2000. <em>Jefferson's West: A Journey with Lewis and Clark</em>.
 Monticello Monograph Series; Monticello Monograph Series. [Charlottesville,
 Va.]: Thomas Jefferson Foundation.
 </p>
 <p>
-        Slaughter, Thomas P. 2003. <em>Exploring Lewis and Clark: Reflections on
+      Slaughter, Thomas P. 2003. <em>Exploring Lewis and Clark: Reflections on
 Men and Wilderness</em>. 1st ed. New York: Knopf : Distributed by Random House.
 </p>
 <p>
-        Swagerty, William R. 2012. <em>The Indianization of Lewis and
+      Swagerty, William R. 2012. <em>The Indianization of Lewis and
 Clark</em>. Norman, Okl.: Arthur H. Clark Co.
 </p>
 <p>
-        Tubbs, Stephenie Ambrose, and Clay Jenkinson. 2003. <em>The Lewis and
+      Tubbs, Stephenie Ambrose, and Clay Jenkinson. 2003. <em>The Lewis and
 Clark Companion: An Encyclopedic Guide to the Voyage of Discovery</em>. First
 edition. New York: Henry Holt.
 </p>
 <p>
-        Vastag, B. 2003. "Medicine on the Lewis and Clark Trail." <em>JAMA</em>
+      Vastag, B. 2003. "Medicine on the Lewis and Clark Trail." <em>JAMA</em>
 289 (10): 1227–30.<a href="https://doi.org/doi:10.1001/jama.289.10.1227">
 https://doi.org/doi:10.1001/jama.289.10.1227</a>.
 </p>
 <p>
-        Woodger, Elin., and Brandon. Toropov. 2004. <em>Encyclopedia of the
+      Woodger, Elin., and Brandon. Toropov. 2004. <em>Encyclopedia of the
 Lewis and Clark Expedition</em>. Facts on File Library of American History;
 Facts on File Library of American History. New York: Facts On File.
 </p>
+<p>
+Map Layers
+</p>
+<p>
+Base Layer: Stamen, Stamen Terrain, 2018,
+http://maps.stamen.com/#toner/12/37.7707/-122.3781
+</p>
+<p>
+1803 Political: Marlene Guzman, 1803 Political Map of North America, 2018.
+</p>
+<p>
+Historic Campsites (used to determine many of the waypoint locations): Robert
+Bergatino, Lewis and Clark Historic Campsites, provided by Ryan Cooper, National
+Park Service, <a href="mailto:ryan_m_cooper@nps.gov">ryan_m_cooper@nps.gov</a>
+</p>
+<p>
+Inbound/Outbound Routes: National Park Service, Lewis and Clark Trail Historic
+Route, National Park Service, March 27, 2017,
+https://www.arcgis.com/home/item.html?id=8a5d4ac482ee4b24baca6dbab5422dbc
+</p>
+<p>
+Native American Tribes: William C. Sturtevant, U.S. Geological Survey. National
+atlas. Indian tribes, cultures & languages: [Unied States] Reston, Va: Interior,
+Geological Survey, 1991. <a
+href="https://www.loc.gov/item/95682185/">https://www.loc.gov/item/95682185/</a>
+(information from map adapted to vector shapefile).
+</p>
+<p>
+Streams and Tributaries: National Park Service, Lewis and Clark Trail Historic
+Route, National Park Service, March 27, 2017, <a
+href="https://www.arcgis.com/home/item.html?id=ab2c0cb4c8824c5e85114cc272334bc1">https://www.arcgis.com/home/item.html?id=ab2c0cb4c8824c5e85114cc272334bc1</a>
+</p>
 </div>
-//put text above here as HTML 
+//put text above here as HTML
 ]
